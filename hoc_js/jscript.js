da58a898a6d3bf47097a530af571e8f264f3ec33
Huynh Chi Dat <0337937146hcd@gmail.com>
2025-05-10 14:02:18 +0700

    lần đầu

---
+++ hoc_js/jscript.js
@@ -0,0 +1,23 @@
+// function tich(a, b) {
+//   return a * b;
+// }
+
+// alert("Chao cac ban toi la Dat Dep Trai");
+// confirm("Ok hay không");
+
+alert("Mình là Đạt. Mình mới học Web");
+
+// các kiểu khai báo biến
+var name;
+let tuoi;
+// const không thể thay đổi
+const nuocSoi = 100;
+//gán giá trị cho biến
+tuoi = 10;
+console.log("Tuổi bạn là: " + tuoi + " tuổi");
+name = prompt("Nhập tên của bạn: ");
+console.log(name);
+console.log(nuocSoi);
+
+tuoi = 15;
+console.log(tuoi);
